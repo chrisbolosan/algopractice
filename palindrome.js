ceec678db3016d134bcb8188102b4c5126d771f6
chrisbolosan <cbolosan@gmail.com>
2021-09-28 15:15:30 -0500

    first commit

---
+++ palindrome.js
@@ -0,0 +1,23 @@
+// isPalindrome 
+//Check if string is palindrome
+// examples car => rac  false
+// taat =>taat  true
+function isPalindrome(str) {
+    //if less than 1 then false
+   while (str.length>1) {
+    let first = str[0].toLowerCase()
+    let last = str[str.length-1].toLowerCase()
+    
+      if (first !== last) {
+        return false
+      } else {
+          str.slice(1,str.length-1) 
+  
+      } return true
+    }
+  
+   }
+  isPalindrome('!? 100 ABCcba 001 ?!')
+  console.log(isPalindrome('car'))
+  console.log(isPalindrome('racecar'))
+  console.log(isPalindrome('RaCecAr'))

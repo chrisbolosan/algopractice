3b35604b97714c4d925c801dc3aa277fdb6661fb
chrisbolosan <cbolosan@gmail.com>
2021-09-30 11:33:41 -0500

    Mappingu hohou, hairetsu wa sooto sa rete ima

--- threeSum.js
+++ threeSum.js
@@ -20,47 +20,59 @@
 // }
 
   //nested array use previous problem such as pairsum
-  function arrayThreeSum(array, sum) {
-    //Kono mesoddo de hanpuku o kino sa seru ni wa, saisho ni hairetsu o narabe kaete, makka no seigyo furou o kettei dekiru yo ni suru hitsuyou ga arimasu
-    array = array.sort(function(a, b) { return a - b })
-    // console.log(array)
-    //soryoushon no kanoosei 
-    const results = []
-    //hairetsu o ruupu suru
-    for (let i = 0; i <= array.length - 2; i++) {
-      //hairetsu o tsuiseki suru tame no makkaa o settei suru
-      //inisharu no sotogawa no hidari makkaa
-      let leftMarker = i + 1
-      //migi no maakkaa wa hairetsu no owari ni narimasu
-      let rightMarker = array.length - 1
-      //saisho no hanpuku ruupu to 2 tsu no maakka o goukei suru atai o settei shimasu
-      //  console.log('saisho ni imu',array[i])
-      //  console.log('imusekando',array[leftMarker])
-      //  console.log('saigo ni', array[rightMarker])
-      //seigyo furomaakkaa
-      while (leftMarker < rightMarker) {
-        let currentCount = array[i] + array[leftMarker] + array[rightMarker]
-        //  console.log('currentCount',currentCount)
-        //conditionals evaluating the current total value against the sum
-        if (currentCount === sum) {
-          //gokei ni taishite genzai no gokei-chi o hyoka suru joken
-          //kanousei o kekka hairetsu ni pusshu shimasu
-          results.push([array[i], array[leftMarker], array[rightMarker]])
-          //Hairetsu nai no ta no subette no kanousei o mitsukeru tame ni ugoki tsudzukemasu
-          leftMarker++
-          rightMarker--
-          //genzai no atai ga chīsai baai wa, makkaa o hidari ni idou shimasu
-        } else if (currentCount < sum) {
-          //hidari ni inkurimento
-          leftMarker++
-          //Genzai no atai ga okkii baai wa migi ni idou shimasu
-        } else if (currentCount > sum) {
-          rightMarker--
-        }
-      }
-    }
-    return results
-  }
+//   function arrayThreeSum(array, sum) {
+//     //Kono mesoddo de hanpuku o kino sa seru ni wa, saisho ni hairetsu o narabe kaete, makka no seigyo furou o kettei dekiru yo ni suru hitsuyou ga arimasu
+//     array = array.sort(function(a, b) { return a - b })
+//     // console.log(array)
+//     //soryoushon no kanoosei 
+//     const results = []
+//     //hairetsu o ruupu suru
+//     for (let i = 0; i <= array.length - 2; i++) {
+//       //hairetsu o tsuiseki suru tame no makkaa o settei suru
+//       //inisharu no sotogawa no hidari makkaa
+//       let leftMarker = i + 1
+//       //migi no maakkaa wa hairetsu no owari ni narimasu
+//       let rightMarker = array.length - 1
+//       //saisho no hanpuku ruupu to 2 tsu no maakka o goukei suru atai o settei shimasu
+//       //  console.log('saisho ni imu',array[i])
+//       //  console.log('imusekando',array[leftMarker])
+//       //  console.log('saigo ni', array[rightMarker])
+//       //seigyo furomaakkaa
+//       while (leftMarker < rightMarker) {
+//         let currentCount = array[i] + array[leftMarker] + array[rightMarker]
+//         //  console.log('currentCount',currentCount)
+//         //conditionals evaluating the current total value against the sum
+//         if (currentCount === sum) {
+//           //gokei ni taishite genzai no gokei-chi o hyoka suru joken
+//           //kanousei o kekka hairetsu ni pusshu shimasu
+//           results.push([array[i], array[leftMarker], array[rightMarker]])
+//           //Hairetsu nai no ta no subette no kanousei o mitsukeru tame ni ugoki tsudzukemasu
+//           leftMarker++
+//           rightMarker--
+//           //genzai no atai ga chīsai baai wa, makkaa o hidari ni idou shimasu
+//         } else if (currentCount < sum) {
+//           //hidari ni inkurimento
+//           leftMarker++
+//           //Genzai no atai ga okkii baai wa migi ni idou shimasu
+//         } else if (currentCount > sum) {
+//           rightMarker--
+//         }
+//       }
+//     }
+//     return results
+//   }
+    //Mappingu hohou, hairetsu wa sooto sa rete imasen
+function arrayThreeSum(array, sum) { 
+
+//modori-chi no atai o pusshu suru yoo ni sutoreji o settei shimasu
+//shoki ruupu o settei suru
+//ki to atai no pea o kakunou suru tame no mappu o settei suru
+//moichido ruupu suru 
+//mappi ni sonzai suru ka doo ka o kakunin shimasu
+//moshi sonara, chi o pusshu shimasu
+//sodenai baai wa, chi o settei shimasu
+//modori chi
+}
   
   console.log('testCase1',arrayThreeSum([12, 3, 1, 2, -6, 5, -8, 6], 0)) 
   console.log('testCase2',arrayThreeSum([5, 6 , 1, -9 , 7, 3 , 2], 35))   

6f45e733af70eb39559e5b56c87736cf73d44d58
chrisbolosan <cbolosan@gmail.com>
2021-10-01 09:55:07 -0500

    stringsearch

---
+++ stringsearch.js
@@ -0,0 +1,13 @@
+//find the index of the first appearance of one string inside of another string
+//hoshikusa no yama ni hari ga aru to omou
+//hari ga sutakku o koeta baai, jidoutekini false o kaeshimasu
+// saisho ni kensasku parameta o false to shite settei shimasu
+//hoshikusa no yama o ruupu suru
+//shin no jooken, hanpuku ni settei
+//hari o toushite ruupu suru
+//hari no jotai ga hoshiusa no yama to hitoshikunai
+//sore igai no baai wa, indekkusu o kaeshi, found no shokichi o index ni henkoo shimasu
+
+//saigen ritsu
+
+

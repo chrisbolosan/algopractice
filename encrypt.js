e1e1fe4569c03459ff25b6e6b16bf0cd945620fc
chrisbolosan <cbolosan@gmail.com>
2021-10-02 11:08:22 -0500

    pseudo

--- encrypt.js
+++ encrypt.js
@@ -1,2 +1,4 @@
 //prompt
 //write a function that will change all characters of a string to '#' but last 4
+
+//ruupu suru koto wa dekumasuga, hissude wa arimasen. Subete o -4 ni suraisu shite kara, seiki hyougen de subete no moji o pondo ni okikaete kara, mo ichido suraisu shimas

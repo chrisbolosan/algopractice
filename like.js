52a0786d38d9bf4924810897741436f6136d242f
chrisbolosan <cbolosan@gmail.com>
2021-10-03 10:38:42 -0500

    newproblem

---
+++ like.js
@@ -0,0 +1,7 @@
+//prompt implement a function that takes an array that contains names of people that like something.
+/* examples 
+[]                                -->  "no one likes this"
+["Michael"]                         -->  "Peter likes this"
+["Manny", "Alfred"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alfred", "Michael", "Manny", "Max"]  -->  "Alex, Jacob and 2 others like this"*/

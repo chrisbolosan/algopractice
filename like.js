81bec87305998d59d62ad940b01a4887cf211612
chrisbolosan <cbolosan@gmail.com>
2021-10-03 10:59:51 -0500

    solution lazy way

--- like.js
+++ like.js
@@ -5,13 +5,25 @@
 ["Manny", "Alfred"]                 -->  "Jacob and Alex like this"
 ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 ["Alfred", "Michael", "Manny", "Max"]  -->  "Alex, Jacob and 2 others like this"*/
-
-// namae nashi
-
-//tan itsu no namae
-
-//to de ketsugo sa reta 2 tsu no namae
-
-//to de ketsugo sa reta 3 tsu no namae
-
-//saisho no 2tsu ga ando to number de tsudzura rete iru 4 tsu no namae
+function likes(names){
+    // namae nashi
+    if (names.length===0) {
+        return 'No one likes this'
+    //tan itsu no namae
+    } if (names.length ===1) {
+        return `${names[0]} likes this`
+    //to de ketsugo sa reta 2 tsu no namae
+    } if (names.length === 2) {
+        return `${names[0]} and ${names[1]} like this}`
+    //to de ketsugo sa reta 3 tsu no namae
+    } if (names.length ===3) {
+        return `${names[0]}, ${names[1]}, and ${names[2]} like this`
+    //saisho no 2tsu ga ando to number de tsudzura rete iru 4 tsu no namae
+    } if (names.length >3) {
+        return `${names[0]}, ${names[1]}, and $({names.length}-2) like this`
+}
+console.log( likes([]))
+console.log( likes(["Michael"]))
+console.log( likes(["Manny", "Alfred"]))
+console.log( likes(["Max", "John", "Mark"]))
+console.log( likes(["Alfred", "Michael", "Manny", "Max"]))

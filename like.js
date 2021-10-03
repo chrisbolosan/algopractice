c100dd142e83f1262c62d0e2740d7ec6e6a8b178
chrisbolosan <cbolosan@gmail.com>
2021-10-03 10:44:11 -0500

    logic

--- like.js
+++ like.js
@@ -5,3 +5,13 @@
 ["Manny", "Alfred"]                 -->  "Jacob and Alex like this"
 ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 ["Alfred", "Michael", "Manny", "Max"]  -->  "Alex, Jacob and 2 others like this"*/
+
+// namae nashi
+
+//tan itsu no namae
+
+//to de ketsugo sa reta 2 tsu no namae
+
+//to de ketsugo sa reta 3 tsu no namae
+
+//saisho no 2tsu ga ando to number de tsudzura rete iru 4 tsu no namae

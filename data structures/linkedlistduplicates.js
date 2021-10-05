bf1878810252335b41344cb4798be89e1be04f24
chrisbolosan <cbolosan@gmail.com>
2021-10-05 10:16:43 -0500

    data structures linked lists

---
+++ data structures/linkedlistduplicates.js
@@ -0,0 +1,8 @@
+//prompt create a function that takes a list in increasing order and delete any duplicates in the list
+//solve in non recursive way
+//if list is null, just return null
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}

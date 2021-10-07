80c406cac3a7303297c7b6477eda1cdc2fae7076
chrisbolosan <cbolosan@gmail.com>
2021-10-07 13:58:08 -0500

    osu singlelinkedlist

--- data structures/singlelinkedlist.js
+++ data structures/singlelinkedlist.js
@@ -4,6 +4,12 @@ class Node {
     this.next = next;
   }
 }
+//osu
+//tanitsu no risuto o pusshu
+//kansuu ni watasa reta atai o shiyou shite atarshi nodo o sakusei shimasu
+//heddo matawa teru chi ga nai baai wa, atarashiku sakusei sa reta noodo to hitoshiku naru yoo ni nioodo o settei shimasu
+// sore igai no baai wa, teru no puropati o atarashiku sakusei sa reta noodo ni settei shi, teiru no atashiku sakusei sa reta noodo ni koushin shimasu
+//naga sa o 1su inkurimento shimasu
 class SinglyLinkedList {
   constructor() {
     this.head = null;

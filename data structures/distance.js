f2bff54437760d701fb2ab2ee232eff4f72a0a4b
chrisbolosan <cbolosan@gmail.com>
2021-10-07 13:50:58 -0500

    distance done with 2 points

--- data structures/distance.js
+++ data structures/distance.js
@@ -1,2 +1,16 @@
 //find distance between 2 points when given (x,y) coordinates
 //point 1 (5,5) , point 2(10,10)
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  static distance(a, b) {
+    dx = a.x - b.x;
+    dy = a.y - b.y;
+  }
+}
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+
+Point.distance(p1, p2);

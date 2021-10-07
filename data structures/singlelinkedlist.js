456edf027c3355350d04e8396f00ddfa9d52e6d1
chrisbolosan <cbolosan@gmail.com>
2021-10-07 13:34:47 -0500

    fixed value and set new function

--- data structures/singlelinkedlist.js
+++ data structures/singlelinkedlist.js
@@ -1,6 +1,13 @@
 class Node {
   constructor(val) {
+    this.val = val;
+    this.next = next;
+  }
+}
+class SinglyLinkedList {
+  constructor() {
     this.head = null;
     this.tail = null;
+    this.length = 0;
   }
 }

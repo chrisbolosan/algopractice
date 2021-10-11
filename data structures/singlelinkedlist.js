ae5c0307c3d78ffb5536e2f80a92f189c8dc20d9
chrisbolosan <cbolosan@gmail.com>
2021-10-11 11:06:31 -0500

    push

--- data structures/singlelinkedlist.js
+++ data structures/singlelinkedlist.js
@@ -233,4 +233,16 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (!head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }

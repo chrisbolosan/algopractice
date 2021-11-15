c71840bad296f20a28aac4cacc854dfb18a4d941
chrisbolosan <cbolosan@gmail.com>
2021-11-15 14:20:00 -0600

    repush

--- LeetCode/Javascript Solutions/TwoSum.js
+++ LeetCode/Javascript Solutions/TwoSum.js
@@ -1,1 +1,12 @@
 //#1
+//hashing method solution
+var twoSum = function (nums, target) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (hash[diff] !== undefined) {
+      return [hash[diff], i];
+    }
+    hash[nums[i]] = i;
+  }
+};

2ea7e3e1c6e1fa23fb0bdfdea7440f1b4c9cd423
chrisbolosan <cbolosan@gmail.com>
2021-11-15 14:12:56 -0600

    refactor

--- data structures/trees/TwoSum.js
+++ data structures/trees/TwoSum.js
@@ -1,7 +1,12 @@
 // Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
-//Input: root = [5,3,6,2,4,null,7], k = 9
+//Input: root = [5,3,6,2,4,null,-7], k = 4
 // Output: true
 
+// function TreeNode(val, left, right) {
+//         this.val = (val===undefined ? 0 : val)
+//      this.left = (left===undefined ? null : left)
+//         this.right = (right===undefined ? null : right)
+//     }
 function findTarget(root, k) {
   let queue = [root];
   let map = {};

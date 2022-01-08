7bf47ae573e469ba6877b151d27a1648bdbb5719
chrisbolosan <cbolosan@gmail.com>
2022-01-08 16:50:13 -0600

    faster

--- LeetCode/Javascript Solutions/RemoveDuplicatesfromArray.js
+++ LeetCode/Javascript Solutions/RemoveDuplicatesfromArray.js
@@ -2,11 +2,10 @@
 const removeDuplicates = function (nums) {
   if (nums.length <= 1) return nums.length;
   const i = 0;
-  for (let j = 1; j < nums.length; j++) {
+  for (let j = 1; j <= nums.length; j++) {
     if (nums[i] !== nums[j]) {
-      i++;
-      nums[i] = nums[j];
+      nums[++i] = nums[j];
     }
   }
-  return i + 1;
+  return i;
 };
